abc1b0bfd3f210cb949f1a4a80a293f14663c82f
Hans Fugal <hans@fugal.net>
2024-09-20 11:16:32 -0700

    red;stopToggle

--- Jamb.cc
+++ Jamb.cc
@@ -13,6 +13,9 @@ void Jamb::dispatch(Command cmd) {
     case Command::Type::RecallPreset:
       aeolus_.programChange(cmd.u.preset);
       break;
+    case Command::Type::StopToggle:
+      aeolus_.stopToggle(cmd.u.stop.group, cmd.u.stop.button);
+      break;
     default:
       LOGf("event type %d not yet handled", cmd.type);
   }
--- test/Jamb.cc
+++ test/Jamb.cc
@@ -27,3 +27,18 @@ TEST(Jamb, recallPreset) {
   lpMidi.emit({0xB0, {0x69, 0x7f}});
   ApprovalTests::Approvals::verifyAll({lpMidi, aeolusMidi});
 }
+
+TEST(Jamb, stopToggle) {
+  FakeMidi lpMidi, aeolusMidi;
+  auto launchpad = Launchpad{lpMidi};
+  launchpad.init();
+  auto aeolus = Aeolus{aeolusMidi};
+  Jamb jamb{launchpad, aeolus};
+  jamb.init();
+  lpMidi.messages_.clear();
+  aeolusMidi.messages_.clear();
+
+  lpMidi.emit({0x90, {0x17, 0x7f}});  // group 1 button 15
+  lpMidi.emit({0x90, {0x42, 0x7f}});  // group 2 button 2
+  ApprovalTests::Approvals::verifyAll({lpMidi, aeolusMidi});
+}
