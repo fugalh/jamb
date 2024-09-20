f8f5ac1777c2902233778df8328999cbd5a4f70f
Hans Fugal <hans@fugal.net>
2024-09-20 12:57:15 -0700

    Do not perturb aeolus on startup
    Now that it's fully functional, no need to reset aeolus to make sound on startup. Just leave it be.

--- linux/main.cc
+++ linux/main.cc
@@ -16,13 +16,6 @@ int main(void) {
   launchpad.init();
   jamb.init();
 
-  aeolus.generalCancel();
-  aeolus.stopOn(1, 0);  // I prin8 (channel 0)
-  aeolus.stopOn(2, 3);  // III suab8 (channel 1)
-  aeolus.stopOn(2, 4);  // III rofl4 (channel 1)
-  // Less annoying: `watch pmidi -p 128:0 ~/bwv651.mid`
-  // aeolus.noteOn(0, 69);
-
   seq.launchpad_->readLoop();
   return 0;
 }
