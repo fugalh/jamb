16a1fe951293f42d027978dbb93a318883d06952
Hans Fugal <hans@fugal.net>
2024-10-18 17:34:50 -0700

    regenerate build.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,5 +1,4 @@
 if [ ! -d .tup ]; then
-    mkdir -p build build/test build/linux bin
     tup init
     { echo "[updater]"; echo "num_jobs=1"; } >> .tup/options
     echo "bootstrapped tup"
@@ -7,5 +6,5 @@ else
     echo "tup already bootstrapped"
 fi
 if [ `uname` = 'Darwin' ]; then
-    buck2 clean
+    buck2 kill
 fi
