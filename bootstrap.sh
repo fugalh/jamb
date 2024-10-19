3684edd6c4a4d58358d22d2df63890f707ca0222
Hans Fugal <hans@fugal.net>
2024-10-18 17:27:12 -0700

    tweak bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
 if [ ! -d .tup ]; then
-        mkdir -p build build/test build/linux bin
-	tup init
-        { echo "[updater]"; echo "num_jobs=1"; } >> .tup/options
-	echo "bootstrapped"
+    mkdir -p build build/test build/linux bin
+    tup init
+    { echo "[updater]"; echo "num_jobs=1"; } >> .tup/options
+    echo "bootstrapped tup"
 else
-	echo "already bootstrapped"
+    echo "tup already bootstrapped"
+fi
+if [ `uname` = 'Darwin' ]; then
+    buck2 clean
 fi
